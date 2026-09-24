a7998ce6cfd450c4c31c915acf6247ce48a9e6c8
wandradec12-debug <wandrade.c12@gmail.com>
2026-09-24 09:39:50 -0300

    Evita cache da versão antiga do sistema

--- web/vite.config.js
+++ web/vite.config.js
@@ -5,6 +5,11 @@ export default defineConfig({
   plugins: [react()],
   preview: {
     host: '0.0.0.0',
-    allowedHosts: true
+    allowedHosts: true,
+    headers: {
+      'Cache-Control': 'no-store, no-cache, must-revalidate, proxy-revalidate',
+      'Pragma': 'no-cache',
+      'Expires': '0'
+    }
   }
 });
